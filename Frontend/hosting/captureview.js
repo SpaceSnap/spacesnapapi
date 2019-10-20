9b8f5b01c0a60e0b64cdbc1de0c4414041db87c5
Garrett S. Curtis <Garrett@cyber-medics.com>
2019-10-20 12:03:44 -0400

    Updates to text

--- Frontend/hosting/captureview.js
+++ Frontend/hosting/captureview.js
@@ -2,8 +2,8 @@ function loadResults(data) {
 	console.log(`Loading results: ${data}`);
 	if (data) {
 		for (var i = 0; i < (data.length - 1) || i < 3; i++) {
-			$('#pic-description' + i).text(`Description: ${data[i].description}`);
-			$('#pic-score' + i).text(`Score: ${(data[i].score.toFixed(2) * 100)}`);
+			$('#pic-description' + i).text(`Description: ${data[i].description}: `);
+			$('#pic-score' + i).text(`Score: ${(data[i].score.toFixed(2) * 100)}%`);
 		}
 	}
 	else {
